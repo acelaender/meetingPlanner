89a8ffaeadc975f9b741e7ca841021a8fafb81d7
Lukas <acelaender@gmail.com>
2025-09-27 16:02:10 +0200

    added repositories and created Entities, added create meeting method

--- src/main/java/at/fhtw/swen2/meetingplanner/bl/model/Note.java
+++ src/main/java/at/fhtw/swen2/meetingplanner/bl/model/Note.java
@@ -1,4 +1,33 @@
 package at.fhtw.swen2.meetingplanner.bl.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull(message = "title cannot be null")
+    @NotBlank(message = "title cannot be blank")
+    private String title;
+    @NotNull(message = "content cannot be null")
+    private String content;
+    @ManyToOne(optional = false)
+    @JoinColumn(name="meeting_id", nullable = false, updatable = false)
+    @Setter
+    private Meeting meeting;
+
+    public Note(String title, String content, Meeting meeting) {
+        this.title = title;
+        this.content = content;
+        this.meeting = meeting;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,15 @@ module at.fhtw.swen2.meetingplanner {
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.persistence;
+    requires jakarta.validation;
     requires static lombok;
+    requires org.antlr.antlr4.runtime;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires spring.data.jpa;
 
 
-    opens at.fhtw.swen2.meetingplanner to javafx.fxml;
+    opens at.fhtw.swen2.meetingplanner to javafx.fxml, spring.core, spring.beans;
     exports at.fhtw.swen2.meetingplanner;
     exports at.fhtw.swen2.meetingplanner.controller;
     opens at.fhtw.swen2.meetingplanner.controller to javafx.fxml;
